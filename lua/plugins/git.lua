4d796b5ead378852c6d5b016a6283ccf329e43bd
Aquiles Gomez <aquilesg24@gmail.com>
2025-08-29 06:48:42 -0700

    Add git lensline

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,6 +2,14 @@ vim.treesitter.language.register("markdown", "octo")
 
 return {
   {
+    "oribarilan/lensline.nvim",
+    tag = "1.0.0", -- or: branch = 'release/1.x' for latest non-breaking updates
+    event = "LspAttach",
+    config = function()
+      require("lensline").setup()
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     keys = {
       {
